9fa21c9311bf4b2b234ba685d9651971ba64136f
Hayato Nishimura <nhayato@nhayato.com>
2022-02-10 13:40:49 +0900

    Add another ans for fence repair (p.75)

--- fence_repair/fence_repair/__init__.py
+++ fence_repair/fence_repair/__init__.py
@@ -1,3 +1,5 @@
+from heapq import heappop, heappush
+
 __version__ = "0.1.0"
 
 
@@ -15,10 +17,28 @@ def solve(N: int, L: list[int]) -> int:
     return ans
 
 
+def solve_by_priority_queue(N: int, L: list[int]) -> int:
+    ans = 0
+    priority_queue = []
+    for i in range(N):
+        heappush(priority_queue, L[i])
+
+    while len(priority_queue) > 1:
+        # 一番短い板、次に短い板を取り出す
+        l1 = heappop(priority_queue)
+        l2 = heappop(priority_queue)
+
+        ans += l1 + l2
+        heappush(priority_queue, l1 + l2)
+
+    return ans
+
+
 def main():
     N = 3
     L = [8, 5, 8]
     print(solve(N, L))
+    print(solve_by_priority_queue(N, L))
 
 
 if __name__ == "__main__":
--- fence_repair/tests/test_fence_repair.py
+++ fence_repair/tests/test_fence_repair.py
@@ -1,4 +1,4 @@
-from fence_repair import __version__, solve
+from fence_repair import __version__, solve, solve_by_priority_queue
 
 
 def test_good():
@@ -11,6 +11,18 @@ def test_good2():
     N = 5
     L = [3, 4, 5, 1, 2]
     assert solve(N, L) == 33
+
+
+def test_queue_good():
+    N = 3
+    L = [8, 5, 8]
+    assert solve_by_priority_queue(N, L) == 34
+
+
+def test_queue_good2():
+    N = 5
+    L = [3, 4, 5, 1, 2]
+    assert solve_by_priority_queue(N, L) == 33
 
 
 def test_version():
